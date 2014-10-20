f53999236c9402bcb48e9963a620166ba4e956f8
Evgeny Anoykin <hamway0611@gmail.com>
2014-10-21 01:51:49 +0600

    add config

---
+++ src/config/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hamway
+ * Date: 21.10.14
+ * Time: 1:48
+ */
+return array(
+	'timeout'   => 50,				// Max curl timeout
+	'userAgent' => true,			// Generate useragent
+	'sleep'     => [				// pause between requests in seconds
+		'min' => 0,
+		'max' => 0
+	],
+	'proxy'     => false,			// use proxy
+	'proxyList' => [				// proxy list
+		'118.97.194.49:8080'
+	]
+);
